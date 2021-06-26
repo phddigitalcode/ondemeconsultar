0b700cf763b1f04f3486830cd43d2f5a0531b37a
Phelipp Nascimento <phelippnd.programador@gmail.com>
2021-06-26 19:17:24 -0300

    Inicio Projeto

---
+++ src/main/java/br/com/phddigitalcode/ondemeconsultar_api/OndemeconsultarApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.phddigitalcode.ondemeconsultar_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndemeconsultarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OndemeconsultarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/phddigitalcode/ondemeconsultar_api/OndemeconsultarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phddigitalcode.ondemeconsultar_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OndemeconsultarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
